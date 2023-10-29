e65497f43658b7e0704bde3b7c67ddd2750ba4af
nicorey89 <nicolasthian8907@gmail.com>
2023-10-29 19:34:58 -0300

    acomodando la vista desktop

--- main.js
+++ main.js
@@ -4,3 +4,31 @@ const toogleMenu = document.querySelector(".burgerMenu");
 btnMenu.addEventListener("click", () => {
     toogleMenu.classList.toggle("toogleMenu")
 });
+
+const btnShop = document.querySelector(".shop-btn");
+const btnNew = document.querySelector(".new-btn");
+const shop = document.querySelector(".shop");
+const newColection = document.querySelector(".newColection");
+
+btnShop.addEventListener("click", () => {
+    shop.classList.toggle("active-shop")
+})
+btnNew.addEventListener("click", () => {
+    newColection.classList.toggle("active-new")
+})
+
+const favorite1 = document.querySelector("#favorite1");
+const favorite2 = document.querySelector("#favorite2");
+const favorite3 = document.querySelector("#favorite3");
+
+favorite1.addEventListener("click", () => {
+    favorite1.classList.toggle("fa-solid")
+})
+
+favorite2.addEventListener("click", () => {
+    favorite2.classList.toggle("fa-solid")
+})
+
+favorite3.addEventListener("click", () => {
+    favorite3.classList.toggle("fa-solid")
+})
